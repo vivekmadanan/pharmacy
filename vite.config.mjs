5e77e1c79f59f0f93a80e90eb5887ce4dee9c419
vivekmadanan <vivekmadanan2008@gmail.com>
2025-11-25 15:49:03 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,8 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pharmacy/',   // your repo name
+  base: '/pharmacy/',   // replace with your repo name
   plugins: [react()],
 })
